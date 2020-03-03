db0f385144bc02eafbab40306448c3be4e63ed8f
barucharky <benarky@gmail.com>
2020-03-03 16:52:01 +0200

    practice with servers

--- 01_http_requests/01_simple_server/main.go
+++ 01_http_requests/01_simple_server/main.go
@@ -8,28 +8,21 @@ import (
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(
 		writer,
-		"Hello world, %s!",
+		"Hello, %s!",
 		request.URL.Path[1:],
 	)
 }
 
-func pinky(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(
-		writer,
-		"NARF!",
-	)
-}
-
-func brain(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(
-		writer,
-		"Same thing we do every night, Pinky; try to take over the world!",
-	)
-}
-
 func main() {
-	http.HandleFunc("/hello", handler)
-	http.HandleFunc("/pinky", pinky)
-	http.HandleFunc("/brain", brain)
+	http.HandleFunc("/", handler)
+
+	// -- ------------------------------
+	/*
+		func ListenAndServe(addr string, handler Handler) error
+			- It listens on the TCP network `addr`
+			- It then calls Serve with `handler` to handle requests on incoming connections.
+		The handler is typically nil, in which case the DefaultServeMux is used.
+	*/
+
 	http.ListenAndServe(":8090", nil)
 }
--- 01_http_requests/02_multiple_handlers/server.go
+++ 01_http_requests/02_multiple_handlers/server.go
@@ -5,21 +5,40 @@ import (
 	"net/http"
 )
 
-func hello(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello, World!")
+func pinky(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "NARF!")
 }
 
-func world(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "World, Hello!")
+func brain(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Try to take over the world!")
 }
 
 func main() {
+
+	// -- ------------------------------
+	/*
+			Defines parameters for running an HTTP server.
+			The Server struct configuration:
+		    type Server struct {
+		        Addr           string
+		        Handler        Handler
+		        ReadTimeout    time.Duration
+		        WriteTimeout   time.Duration
+		        MaxHeaderBytes int
+		        TLSConfig      *tls.Config
+		        TLSNextProto   map[string]func(*Server, *tls.Conn, Handler)
+		        ConnState      func(net.Conn, ConnState)
+		        ErrorLog       *log.Logger
+		    }
+			For details see: https://pkg.go.dev/net/http?tab=doc#Server
+	*/
+
 	server := http.Server{
 		Addr: "127.0.0.1:8090",
 	}
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/world", world)
+	http.HandleFunc("/pinky", pinky)
+	http.HandleFunc("/brain", brain)
 
 	server.ListenAndServe()
 }
