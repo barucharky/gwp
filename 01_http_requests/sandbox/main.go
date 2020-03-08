59df47b12aea2c5907dd94b9ddf28fd90481a95e
barucharky <benarky@gmail.com>
2020-03-08 16:08:29 +0200

    practice with servers

--- 01_http_requests/sandbox/main.go
+++ 01_http_requests/sandbox/main.go
@@ -1,3 +1,5 @@
+// B''H
+
 package main
 
 import (
@@ -5,27 +7,46 @@ import (
 	"net/http"
 )
 
-func narf(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "NARF!")
+func pinky(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "NARF!!!")
 }
 
-func pondering(writer http.ResponseWriter, request *http.Request) {
+func brain(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Are you pondering what I'm pondering?")
 }
 
+func dunno(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "I dunno, Brain. Do we have enough %s for that?", request.URL.Path[7:])
+}
+
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Instead of %s, try /pinky or /brain", request.URL.Path[1:])
+	fmt.Fprintf(writer, "Same thing we do every night, %s. Try to take over the world!", request.URL.Path[1:])
 }
 
 func main() {
 
-	var server http.Server = http.Server{
-		Addr: "127.0.0.1:8090",
-	}
+	// -- ------------------------------
+	/*
+			Defines parameters for running an HTTP server.
+			The Server struct configuration:
+		    type Server struct {
+		        Addr           string
+		        Handler        Handler
+		        ReadTimeout    time.Duration
+		        WriteTimeout   time.Duration
+		        MaxHeaderBytes int
+		        TLSConfig      *tls.Config
+		        TLSNextProto   map[string]func(*Server, *tls.Conn, Handler)
+		        ConnState      func(net.Conn, ConnState)
+		        ErrorLog       *log.Logger
+		    }
+			For details see: https://pkg.go.dev/net/http?tab=doc#Server
+	*/
 
-	http.HandleFunc("/pinky", narf)
-	http.HandleFunc("/brain", pondering)
+	http.HandleFunc("/pinky", pinky)
+	http.HandleFunc("/brain", brain)
+	http.HandleFunc("/dunno/", dunno)
 	http.HandleFunc("/", handler)
 
-	server.ListenAndServe()
+	http.ListenAndServe(":8090", nil)
 }
